94d9851cd7afd117a9bd6d0d4a4bde6c84ea5889
Ishan <ishanwijes@gmail.com>
2022-09-17 00:45:16 +1000

    added colours to boxes

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,23 @@
 
 import React from "react"
 import { boxes } from "../boxes"
+import { Box } from "./Box"
+
 
 
 export const App = function(props) {
-    // just like we can add inline styles to the HTML directly, we can add a style directly to the JSX as a property of the html tag
-    // this is called dynamic styles
-    // the reason we would do this is because we can change the style of something dynamically depending on a certain condition
-    // we need a styles object, the key-value pairs represent CSS styles (they need to be camelCase)
-    const stylesObject = {
-        backgroundColor: props.blueMode ? "blue" : "white"
-    }
-
-
-    
     const [boxArray, setBoxes] = React.useState(boxes)
 
+
+
     const jsxArray = boxArray.map(object => {
-        return <div className="empty-square" style={stylesObject} key={object.id}>{object.id}</div>
+        return <Box key={object.id} on={object.on}/>
     })
 
     return (
-    <main>
-        <h1>{jsxArray}</h1>
-    </main>
+        <main>
+            <h1>{jsxArray}</h1>
+        </main>
     )
 }
 
---
+++ src/components/Box.js
@@ -0,0 +1,17 @@
+
+import React from "react"
+import { boxes } from "../boxes"
+
+export const Box = function(props) {
+    // just like we can add inline styles to the HTML directly, we can add a style directly to the JSX as a property of the html tag
+    // this is called dynamic styles
+    // the reason we would do this is because we can change the style of something dynamically depending on a certain condition
+    // we need a styles object, the key-value pairs represent CSS styles (they need to be camelCase)
+    const stylesObject = {
+        backgroundColor: props.on ? "#222" : "none"
+    }
+
+    return (
+        <div className="box" style={stylesObject}></div>
+    )
+}
