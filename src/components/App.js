b068dac6569839359672b612c0d3b0e2bb5aa356
Ishan <ishanwijes@gmail.com>
2022-09-17 22:54:49 +1000

    completed box challenge using .map()

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,42 @@ import { Box } from "./Box"
 
 
 export const App = function(props) {
-    const boxArray = boxes
+    const [boxArray, setBoxArray] = React.useState(boxes)
+    
+    
+
+    // toggleColour function
+    const toggleColour = function(boxId) {
+
+        setBoxArray(prevBoxArray => {
+
+            return prevBoxArray.map(element => {
+                
+                if(element.id === boxId) {
+                    return {
+                        ...element,
+                        on: !element.on
+                    }
+                } else {
+                    return element
+                }
+
+            })
+
+        })
+    }
+
+
 
     const jsxArray = boxArray.map(object => {
-        return <Box key={object.id} on={object.on}/>
+        return ( 
+        <Box 
+        key={object.id} 
+        on={object.on} 
+        toggleColour={toggleColour} 
+        boxId={object.id}
+        />
+        )
     })
 
     return (
--- src/components/Box.js
+++ src/components/Box.js
@@ -6,23 +6,20 @@ import { boxes } from "../boxes"
 
 export const Box = function(props) {
 
-    const [onProperty, setOnProperty] = React.useState(props.on)
-
-    const invertOnProperty = () => {
-        setOnProperty(onProperty => !onProperty)
-    }
-
-
-
     // just like we can add inline styles to the HTML directly, we can add a style directly to the JSX as a property of the html tag
     // this is called dynamic styles
     // the reason we would do this is because we can change the style of something dynamically depending on a certain condition
     // we need a styles object, the key-value pairs represent CSS styles (they need to be camelCase)
     const stylesObject = {
-        backgroundColor: onProperty ? "#222" : "white"
+        backgroundColor: props.on ? "#222" : "white"
     }
 
     return (
-        <div className="box" style={stylesObject} onClick={invertOnProperty}></div>
+        <div 
+        className="box" 
+        onClick={() => props.toggleColour(props.boxId)}
+        style={stylesObject}
+        >
+        </div>
     )
 }
